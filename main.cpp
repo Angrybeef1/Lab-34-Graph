82eb5a4fb303d7a3a79e84fa96acb71f276ddc97
Angrybeef1 <136150862+Angrybeef1@users.noreply.github.com>
2024-11-23 15:31:28 -0800

    step 2 Complete

--- main.cpp
+++ main.cpp
@@ -131,7 +131,7 @@ int main() {
     // Creates a vector of graph edges/weights
     vector<Edge> edges = {
         // (x, y, w) —> edge from x to y having weight w
-        {0,1,12},{0,2,8},{0,3,21},{2,3,6},{2,6,2},{5,6,6},{4,5,9},{2,4,4},{2,5,5}
+        {0,1,12},{0,2,8},{0,3,21},{2,3,6},{2,6,2},{5,6,6},{4,5,9},{1,1,1},{2,2,2},{3,3,3},{4,4,4},{5,5,5},{6,6,6}
     };
 
     // Creates graph
